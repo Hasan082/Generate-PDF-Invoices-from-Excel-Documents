1ae46892f249ade52b0b7c3c2c3109b45c5cc52e
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-01 02:54:42 +0600

    [ADDED] Pdf genartes for every invoices

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+
 
 filePaths = glob.glob("invoices/*.xlsx")
 
 for filePath in filePaths:
     df = pd.read_excel(filePath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    fileName = Path(filePath).stem
+    invoiceNo = fileName.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoices No:- {invoiceNo}", ln=1)
+    pdf.output(f"pdfs/{fileName}.pdf")
